860933b1d741cb1e9f70c64d949a0e3f2e6ada5d
bbuugi <91682368+bbuugi@users.noreply.github.com>
2024-12-17 22:05:40 +0800

    Update environment variables, dependencies, and footer links to reflect rebranding from Tap4 AI to HMT AI. Adjusted placeholder URLs and localized strings across multiple languages. Updated ads and robots.txt for new domain.

--- lib/constants.ts
+++ lib/constants.ts
@@ -21,10 +21,10 @@ export const RevalidateOneMonth = 3600 * 24 * 30;
 export const PAGE_SIZE = 20;
 export const InfoPageSize = 12;
 
-export const FORM_PLACEHOLDER = 'https://tap4.ai/';
+export const FORM_PLACEHOLDER = 'https://hemaotai.com/';
 
 export const WEBSITE_EXAMPLE =
-  '<a href="https://tap4.ai/" title="Tap4 AI Tools Directory">Tap4 AI Tools Diresctory</a>';
+  '<a href="https://hemaotai.com/" title="HMT AI Tools Directory">HMT AI Tools Diresctory</a>';
 
 export const STARTUP_LIST = [
   {
@@ -303,7 +303,7 @@ export const STARTUP_LIST = [
   {
     DA: 38,
     Website: 'Tap4 AI',
-    URL: 'https://tap4.ai/submit',
+    URL: 'https://hemaotai.com/submit',
     Price: 'Free',
     Tag: 'AI',
   },
